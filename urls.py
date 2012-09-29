6b5f697529f05465e95725826696f9754267ff2e
Chris Sinchok <csinchok@theonion.com>
2012-09-29 12:16:52 -0500

    Bugfixes

--- urls.py
+++ urls.py
@@ -1,12 +1,12 @@
 from django.conf.urls.defaults import patterns, include, url
 
 # Uncomment the next two lines to enable the admin:
-# from django.contrib import admin
-# admin.autodiscover()
+from django.contrib import admin
+admin.autodiscover()
 
 urlpatterns = patterns('',
     # Examples:
-    url(r'^', include('twizzlio.code.urls')),
+    url(r'^', include('twizzlio.core.urls')),
 
     # Uncomment the admin/doc line below to enable admin documentation:
     url(r'^admin/doc/', include('django.contrib.admindocs.urls')),

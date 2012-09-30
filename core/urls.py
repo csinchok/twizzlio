bd575ae598526e914b9b68bd6919fe3264e92bab
Chris Sinchok <csinchok@theonion.com>
2012-09-29 20:45:38 -0500

    Profile

--- core/urls.py
+++ core/urls.py
@@ -12,9 +12,9 @@ urlpatterns = patterns('core.views',
     url(r'^about$', TemplateView.as_view(template_name="about.html"), name="about"),
     url(r'^welcome$', TemplateView.as_view(template_name="welcome.html"), name="welcome"),
     
-    (r'^profile$', 'profile'),
     url(r'^corporate$', TemplateView.as_view(template_name="corporate.html"), name="corporate"),
     url(r'^investors$', TemplateView.as_view(template_name="investors.html"), name="investors"),
     url(r'^brands$', TemplateView.as_view(template_name="brands.html"), name="brands"),
 
+    (r'^profile$', 'profile'),
 )
